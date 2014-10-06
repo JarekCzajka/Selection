a299b6ee202f239c477542156b7fdd041c308cac
Jaroslaw Czajka <40681@longroad.ac.uk>
2014-10-06 10:21:40 +0100

    Two completed exercises

---
+++ Month Exercise.py
@@ -0,0 +1,35 @@
+#Jaroslaw Czajka
+#06-10-2014
+#Month Exercise
+
+#This program asks the user for an integer between the range of 1-12 and displyes the month
+
+month=int(input("Please enter the number:"))
+if month==1:
+    print("The month you have entered is January")
+elif month==2:
+    print("The month you have entered is Febuary")
+elif month==3:
+    print("The month you have entered is March")
+elif month==4:
+    print("The month you have entered is April")
+elif month==5:
+    print("The month you have entered is May")
+elif month==6:
+    print("The month you have entered is June")
+elif month==7:
+    print("The month you have entered is July")
+elif month==8:
+    print("The month you have entered is August")
+elif month==9:
+    print("The month you have entered is September")
+elif month==10:
+    print("The month you have entered is October")
+elif month==11:
+    print("The month you have entered is November")
+elif month==12:
+    print("The month you have entered is December")
+else:
+    print("Please enter a number between 1-12")
+
+#The program displyed the answer depending the on the expresion enterd by the user
---
+++ The grade exercise.py
@@ -0,0 +1,12 @@
+#Jaroslaw Czajka
+#06.10.2014
+#The-grade-Exercise
+
+#This program will ask the user for their result marks and it will show their grade
+
+print("This program will show you the grade you have achived in your exam")
+grade=int(input("Please enter the amount of marks you have scored:"))
+if grade <=81 or grade >=100:
+    print("You have an A")
+elif grade <=71 or grade >80:
+    print("You have a B")
